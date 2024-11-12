577513adfafeed08f54808f036bbc0a73c6d0394
HES3V45 <hes3v45@posteitaliane.it>
2024-11-12 14:17:38 +0100

    aggiunto file test.txt e modificato il file setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os.path import join, dirname
 
 
 exec(open(join(dirname(__file__), 'odoo', 'release.py'), 'rb').read())  # Load release variables
-lib_name = 'odoo'
+lib_name = 'odoo_______________________'
 
 setup(
     name='odoo',
