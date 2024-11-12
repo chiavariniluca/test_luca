9cf2e493a2562ccab6b96c22df09eac6af0aec91
Your Name <you@example.com>
2024-11-12 14:09:06 +0100

    PRIMO COMMIT

---
+++ addons/custom/om_hospital/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Hospital Management',
+    'version' : '1.0',
+    'summary': 'Hospital Management SW',
+    'sequence': 10,
+    'description': """Hospital Management SW""",
+    'category': 'Productivity',
+    'website': 'https://www.odoomates.tech',
+    'depends' : [],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
